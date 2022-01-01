2ddbf6313e9bf5b91ddc6eee06fcd19d83e52aae
DaisyB_ell <alina_borz@mail.ru>
2022-01-01 12:25:27 +0300

    add FragmentManager navigation

--- app/src/main/java/com/daisybell/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/daisybell/composition/presentation/GameFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.daisybell.composition.R
 import com.daisybell.composition.databinding.FragmentGameBinding
+import com.daisybell.composition.domain.entity.GameResult
+import com.daisybell.composition.domain.entity.GameSettings
+import com.daisybell.composition.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private lateinit var level: Level
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +34,39 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val gameSettings = GameSettings(15, 15, 50, 7)
+        val gameResult = GameResult(true, 20, 25, gameSettings)
+        binding.tvSum.setOnClickListener { launchGameFinishedFragment(gameResult) }
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object {
+
+        const val NAME = "GameFragment"
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 
 }
