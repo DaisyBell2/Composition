25e5cfab2a8ffd6673b778349d264d91c820a4d4
DaisyB_ell <alina_borz@mail.ru>
2022-01-13 17:00:04 +0300

    safeArgs

--- app/src/main/java/com/daisybell/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/com/daisybell/composition/presentation/ChooseLevelFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.daisybell.composition.R
 import com.daisybell.composition.databinding.FragmentChooseLevelBinding
 import com.daisybell.composition.domain.entity.Level
 
@@ -35,24 +34,14 @@ class ChooseLevelFragment : Fragment() {
     }
 
     private fun launchGameFragment(level: Level) {
-        val args = Bundle().apply {
-            putParcelable(GameFragment.KEY_LEVEL, level)
-        }
-        findNavController().navigate(R.id.action_chooseLevelFragment_to_gameFragment, args)
+        findNavController().navigate(
+            ChooseLevelFragmentDirections.actionChooseLevelFragmentToGameFragment(level)
+        )
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    companion object {
-
-        const val NAME = "ChooseLevelFragment"
-
-        fun newInstance(): ChooseLevelFragment {
-            return ChooseLevelFragment()
-        }
     }
 
 }
